c77a48a3711a7e98b0f931ef310a7306754f86d9
unknown <tien.ndt2229@sinhvien.hoasen.edu.vn>
2020-06-29 11:54:33 +0700

    update4

--- ProjectThucHanh/src/environments/environment.prod.ts
+++ ProjectThucHanh/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
  export const environment = {
-   production: true
+   production: true,
+   firebase: {
+    apiKey: "AIzaSyAVeN2o6Wz6MW6O6aCSHEgk6lBGgxzhVl8",
+    authDomain: "thuchanh-dfa5e.firebaseapp.com",
+    databaseURL: "https://thuchanh-dfa5e.firebaseio.com",
+    projectId: "thuchanh-dfa5e",
+    storageBucket: "thuchanh-dfa5e.appspot.com",
+    messagingSenderId: "737696233875",
+    appId: "1:737696233875:web:1ceede4d21316397515066"  
+  }
  };
 
